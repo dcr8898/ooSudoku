dc3bda706333941432f43f5190bc443fa9d0014d
Damian C. Rossney <dcrossney@gmail.com>
2015-09-20 08:55:57 -0400

    Complete #initialize

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,13 @@
 class Sudoku
 
+  attr_reader :board
+
   def initialize(board_string)
+    @board = Array.new(81, "123456789")
+    board_string.each_with_index do |cell, index|
+      solve_cell(index, cell) if (1..9).include?(cell)}
+    end
+    solve(board)
   end
 
   def solve
