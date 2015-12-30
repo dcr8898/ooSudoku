8bc43c8f3f54f72457608255781639d9b08ff59a
Damian C. Rossney <dcrossney@gmail.com>
2015-12-29 21:17:20 -0500

    Add spec for implicit solutions via solve_cell method

--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -7,17 +7,23 @@ describe Sudoku do
     it "properly loads init_board" do
       sudoku = Sudoku.new(@input_01)
       expect(
-        sudoku.send(:board_loader, @input_02, @blank_board).join
+        sudoku.send(:board_loader, @input_02, @start_board).join
       ).to eq(@solution_02)
     end
   end
 
   describe "#solve_cell" do
-    it "should solve individual cells" do
+    it "should solve individual cells and modify cohorts" do
       sudoku = Sudoku.new(@input_01)
       expect(
-        sudoku.send(:solve_cell, @blank_board, 0, '1')
+        sudoku.send(:solve_cell, @start_board, 0, '1')
       ).to eq(@solve_cell_01)
+    end
+    it "should modify cohorts for implicitly solved cells" do
+      sudoku = Sudoku.new(@input_01)
+      expect(
+        sudoku.send(:solve_cell, @solve_cell_02, 0, '1')
+      ).to eq(@solve_cell_03)
     end
   end
 
